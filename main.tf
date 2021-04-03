a2544ba08a1b08e0cd0e66d1015fbb26097fb58c
Sandeepsarma Josyula <you@example.com>
2021-04-02 21:05:38 -0400

    Sample RG and SG within 2

--- main.tf
+++ main.tf
@@ -17,3 +17,16 @@ resource "azurerm_resource_group" "create_rg_in_east_us" {
   name     = "sample-rg"
   location = "East US"
 }
+
+# Create a storage account
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.create_rg_in_east_us.name
+  location                 = azurerm_resource_group.create_rg_in_east_us.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
